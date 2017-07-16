31512503c019ff60d4d8ab2cd73fb2722de28412
Artur Araújo Santos Ribeiro <arturrj@hotmail.com>
2017-07-16 19:12:14 -0300

    Estrutura

---
+++ PibNP/src/PibNP.Domain/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PibNP.Domain
+{
+    public class Person
+    {
+        #region Properties
+
+        public string Name { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        private Person(string name)
+        {
+            Name = name;
+        }
+
+        #endregion
+
+        #region Factor
+
+        public static class Factor
+        {
+            public static Person Create(string name)
+            {
+                return new Person(name);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ PibNP/src/PibNP.UnitTest/PersonTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PibNP.UnitTest
+{
+    [TestClass]
+    public class PersonTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
